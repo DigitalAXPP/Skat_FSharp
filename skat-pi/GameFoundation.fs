64438cd1b6e7c772582beeb70679a0b8b7617b82
Alexander Piepenhagen <a.piepenhagen@gmx.net>
2025-05-18 22:30:56 +0200

    Finishing basic logic

--- skat-pi/GameFoundation.fs
+++ skat-pi/GameFoundation.fs
@@ -4,7 +4,10 @@ type Suite = Spades | Clubs | Hearts | Diamonds
 type Rank = Seven | Eight | Nine | Dame | King | Ten | Ace | Bube
 type Card = { Rank: Rank ; Suite: Suite }
 type PlayerId = int
-type GameState = { TurnQueue: PlayerId list}
+type GameState = {
+    TurnQueue: PlayerId list
+    TurnCount: int
+}
 type GameSetup =
     {
         FirstPlayer: Card list
@@ -12,10 +15,15 @@ type GameSetup =
         ThirdPlayer: Card list
         Skat: Card list
     }
+type Action =
+    | Bid
+    | Reject
+    | Accept
 
 let allRanks = [Seven ; Eight ; Nine ; Dame ; King ; Ten ; Ace ; Bube]
 let allSuites = [Spades ; Clubs ; Hearts ; Diamonds]
-let initialState = { TurnQueue = [1; 2; 3] }
+let gamestate = true
+let initialState = { TurnQueue = [1; 2; 3]; TurnCount = 0 }
 let Deck =
     [ for suite in allSuites do
         for rank in allRanks do
@@ -28,16 +36,48 @@ let shuffleDeck deck =
 let pickCard deck =
     deck |> List.head
 
-let nextTurn (state: GameState) : PlayerId * GameState =
-    match state.TurnQueue with
-    | [] -> failwith "Empty Queue"
-    | current :: rest -> 
-        let newQueue = rest @ [current]
-        current, { state with TurnQueue = newQueue }
-
 let dealInitialHand deck =
     let shuffled = shuffleDeck deck
     let (firstPlayerHand, rest) = List.splitAt 10 shuffled
     let (secondPlayerHand, newDeck) = List.splitAt 10 rest
     let (thirdPlayerHand, skat) = List.splitAt 10 newDeck
     { FirstPlayer = firstPlayerHand; SecondPlayer = secondPlayerHand; ThirdPlayer = thirdPlayerHand; Skat = skat }
+
+let nextTurn (state: GameState) : PlayerId * GameState =
+    match state.TurnQueue with
+    | [] -> failwith "Empty Queue"
+    | current :: rest -> 
+        let newQueue = rest @ [current]
+        current, { state with TurnQueue = newQueue; TurnCount = state.TurnCount + 1 }
+
+let getActionFromConsole (playerId: PlayerId) : Action =
+    printfn "Player %d, enter your action (Bid, Reject, Accept):" playerId
+    match System.Console.ReadLine().Trim().ToLower().Split() with
+    | [| "bid" |] -> Bid
+    | [| "reject" |] -> Reject
+    | [| "accept" |] -> Accept
+    | _ ->
+        printfn "Invalid input, defaulting to wait."
+        Reject
+
+let takeAction (player: PlayerId) (action: Action) (game: GameState) : PlayerId * GameState =
+    match action with
+    | Bid -> 
+        printf "Player %i bids." player
+        nextTurn game
+    | Reject ->
+        printf "Player %i rejects." player
+        nextTurn game
+    | Accept -> 
+        printf "Player %i accepts." player
+        nextTurn game
+
+let rec gameloop (player: PlayerId) (game: GameState) =
+    match game.TurnQueue with
+    | [] ->
+        printfn "All players have quit. Game over."
+    | currentPlayer :: rest ->
+        printfn "Turn %d: Player %d's turn" game.TurnCount currentPlayer
+        let action = getActionFromConsole currentPlayer
+        let playerid, gamestate = takeAction currentPlayer action game
+        gameloop playerid gamestate
--- skat-pi/Program.fs
+++ skat-pi/Program.fs
@@ -1,2 +1,16 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+//printfn "Hello from F#"
+open GameFoundation
+
+[<EntryPoint>]
+let main args =
+    let startingHands = dealInitialHand Deck
+    printf "%A" startingHands
+    //while gamestate do
+    //    let fst, snd = nextTurn initialState
+    //    printf "It's player %i turn" fst
+    //    printf "Write something"
+    //    let x = System.Console.ReadLine()
+    //    printf "%s" x
+    gameloop 1 initialState
+    0
