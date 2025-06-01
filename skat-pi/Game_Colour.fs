f099e69bbf46c70b9306a4bab6276d2b51c68a4b
Alexander Piepenhagen <a.piepenhagen@gmx.net>
2025-06-01 22:38:39 +0200

    Adding comparison function.

---
+++ skat-pi/Game_Colour.fs
@@ -0,0 +1,48 @@
+﻿module GamePlay
+
+open GameFoundation
+
+type Suite = 
+    | Diamonds
+    | Hearts
+    | Clubs
+    | Spades
+type Rank =
+    | Seven
+    | Eight
+    | Nine
+    | Dame
+    | King
+    | Ten
+    | Ace
+    | Bube
+type Card = { Suite: Suite; Rank: Rank }
+type GameSetup =
+    {
+        FirstPlayer: Card list
+        SecondPlayer: Card list
+        ThirdPlayer: Card list
+        Skat: Card list
+    }
+
+let allRanks = [Seven ; Eight ; Nine ; Dame ; King ; Ten ; Ace ; Bube]
+let allSuites = [Spades ; Clubs ; Hearts ; Diamonds]
+
+let Deck =
+    [ for suite in allSuites do
+        for rank in allRanks do
+            yield { Rank = rank ; Suite = suite } ]
+
+let shuffleDeck (deck: Card list) =
+    let rnd = System.Random()
+    deck |> List.sortBy (fun _ -> rnd.Next())
+
+let dealInitialHand deck =
+    let shuffled = shuffleDeck deck
+    let (firstPlayerHand, rest) = List.splitAt 10 shuffled
+    let (secondPlayerHand, newDeck) = List.splitAt 10 rest
+    let (thirdPlayerHand, skat) = List.splitAt 10 newDeck
+    { FirstPlayer = firstPlayerHand; SecondPlayer = secondPlayerHand; ThirdPlayer = thirdPlayerHand; Skat = skat }
+
+let compareCards (firstPlayerCard: Card) (secondPlayerCard: Card) (thirdPlayerCard: Card) : Card =
+    List.max [firstPlayerCard; secondPlayerCard; thirdPlayerCard]
