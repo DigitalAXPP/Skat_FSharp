b734324b10d45e60a7feaba446a96704f57fb17a
Alexander Piepenhagen <a.piepenhagen@gmx.net>
2025-06-14 23:38:29 +0200

    Adding update function for the mutable variable.

--- skat-pi/SkatRules.fs
+++ skat-pi/SkatRules.fs
@@ -10,3 +10,8 @@ let mutable game : GameState option = None
 
 let startGame () =
     game <- Some { Players = ["Alex"; "Lukas"; "Finn"]; Score = 0}
+
+let updateGame (increase : int) =
+    match game with
+    | Some g -> game <- Some { g with Score = g.Score + increase}
+    | None -> printfn "None"
