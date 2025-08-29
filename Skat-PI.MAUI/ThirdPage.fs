293980c4771eb0eebeb55fa906e0b7114cb93e05
Alexander Piepenhagen <a.piepenhagen@gmx.net>
2025-08-29 16:07:57 +0200

    Add the Skat library to show the initial hand.

--- Skat-PI.MAUI/ThirdPage.fs
+++ Skat-PI.MAUI/ThirdPage.fs
@@ -5,6 +5,8 @@ open Microsoft.Maui.Controls
 open Fabulous
 open Fabulous.Maui
 open type Fabulous.Maui.View
+open GamePlay
+open GameFoundation
 
 type Intent =
     | DoNothing
@@ -25,6 +27,44 @@ let view model =
             ContentPage(
                 ScrollView(
                     (VStack (spacing =25.) {
+                        let cardSetup = dealInitialHand Deck
+
+                        Label($"Hello, {playerOne.Player}. You are at position {playerOne.Position}.")
+                            .semantics(SemanticHeadingLevel.Level1)
+                            .font(size = 32.)
+                            .centerTextHorizontal()
+
+                        ListView(cardSetup.FirstPlayer)
+                            (fun card -> 
+                                ViewCell(
+                                    Image((cardToImageName card))
+                                        .height(64.)
+                                ))   
+
+                        Label($"Hello, {playerTwo.Player}. You are at position {playerTwo.Position}.")
+                            .semantics(SemanticHeadingLevel.Level1)
+                            .font(size = 32.)
+                            .centerTextHorizontal()
+
+                        ListView(cardSetup.SecondPlayer)
+                            (fun card -> 
+                                ViewCell(
+                                    Image((cardToImageName card))
+                                        .height(64.)
+                                ))  
+
+                        Label($"Hello, {playerThree.Player}. You are at position {playerThree.Position}.")
+                            .semantics(SemanticHeadingLevel.Level1)
+                            .font(size = 32.)
+                            .centerTextHorizontal()
+
+                        ListView(cardSetup.ThirdPlayer)
+                            (fun card -> 
+                                ViewCell(
+                                    Image((cardToImageName card))
+                                        .height(64.)
+                                ))  
+
                         Button($"1st page - {model.Name}", ReturnFirstPage)
                     }).margin(Thickness(10., 0.))
                 ).verticalScrollBarVisibility(ScrollBarVisibility.Always)
